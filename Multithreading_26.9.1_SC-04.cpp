31728a3ffc46468db612893114e0aadb54d9d75a
Dmitriy Kuchuro <dmku@yandex.ru>
2025-07-17 19:30:41 +0300

    Добавьте файлы проекта.

---
+++ Multithreading_26.9.1_SC-04.cpp
@@ -0,0 +1,12 @@
+﻿// Multithreading_26.9.1_SC-04.cpp: определяет точку входа для приложения.
+//
+
+#include "Multithreading_26.9.1_SC-04.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
